0fab57059c58de6994c19f7ac8536ed89b95ef86
Ivona Pashova <129669807+ivonapashova@users.noreply.github.com>
2023-11-01 15:09:06 +0200

    Create 0007_userprofile.py

---
+++ python_ORM/01.Django Models Basics/01. Person_Skeleton/main_app/migrations/0007_userprofile.py
@@ -0,0 +1,24 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('main_app', '0006_alter_product_created_at'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='UserProfile',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('username', models.CharField(max_length=65, unique=True)),
+                ('first_name', models.CharField(max_length=40, unique=True)),
+                ('last_name', models.CharField(max_length=40, unique=True)),
+                ('email', models.EmailField(default='students@softuni.bg', max_length=254, unique=True)),
+                ('bio', models.TextField(max_length=120)),
+                ('profile_image_url', models.URLField()),
+                ('created_at', models.DateTimeField(auto_now_add=True)),
+            ],
+        ),
+    ]
